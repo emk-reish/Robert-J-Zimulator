127163386525aebf44c3ab6e1fd65b1b6f31335b
emk-reish <emmakarinar@gmail.com>
2018-05-20 14:26:45 -0500

    main framework

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+   [SerializeField] private Text endowmentLabel;
+   [SerializeField] private Text studentCountLabel;
+
+    int totalEndowment; 
+
+    float spendingPerStudent;
+
+    float tuition;
+
+    int numStudents;
+
+    int numAlumni;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        endowmentLabel.text = "$" + totalEndowment.ToString();
+
+        studentCountLabel.text = numStudents.ToString();
+    }
+
+    public void AskAlumniForMoney() {
+        
+    }
+}
---
+++ Assets/DonationsBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonationsBehavior : MonoBehaviour {
+
+    float donationRate;
+
+    float averageAmount;
+
+    int numAlumni;
+
+	float DonationPerCycle
+    {
+        get
+        {
+            return donationRate * averageAmount * numAlumni;
+        }
+    }
+}
